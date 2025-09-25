3394bc70a4463b5c8f16c032a507d3ffd84acc1f
Anavi06-tech <anvik0105@gmail.com>
2025-09-25 09:48:23 +0530

    Create Positive & Negative.cpp

---
+++ Positive & Negative.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter a number: ";
+    cin >> a;
+
+    if (a > 0)
+        cout << "Number is Positive" << endl;
+    else if (a < 0)
+        cout << "Number is Negative" << endl;
+    else
+        cout << "Number is Zero" << endl;
+
+    return 0;
+}
+
+// OUTPUT
+// Enter a number: 11
+// Number is Positive
+// Enter a number: -11
+// Number is Negative
+// Enter a number: 0
+// Number is Zero
